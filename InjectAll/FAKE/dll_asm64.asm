e41136d4102e107de7887c2aae24a6875c99b17d
dennisbabkin.com <dennis_babkin@hotmail.com>
2021-05-29 14:29:59 -0700

    Update dll_asm64.asm

--- InjectAll/FAKE/dll_asm64.asm
+++ InjectAll/FAKE/dll_asm64.asm
@@ -45,65 +45,65 @@ UserModeNormalRoutine PROC
 	; INFO: How to get address of function in base-independent way:
 	;  https://dennisbabkin.com/blog/?t=how-to-implement-getprocaddress-in-shellcode
 
-	mov			[rsp + 08h], rcx		; BaseAddr
+	mov         [rsp + 08h], rcx		; BaseAddr
 
 	; Set the stack frame & save nonvolatile registers
 	;
-	mov			[rsp + 10h], rdi
-	mov			[rsp + 18h], rbp
-	sub			rsp, 48h
+	mov         [rsp + 10h], rdi
+	mov         [rsp + 18h], rbp
+	sub         rsp, 48h
 
-	mov			rdi, rdx				; DllName
+	mov         rdi, rdx                            ; DllName
 
 	; Get DllBase for ntdll.dll
 	;
-	mov			rax, gs:[60h]			; rax = ProcessEnvironmentBlock
-	mov			rax, [rax+18h]			; rax = Ldr
-	mov			rax, [rax+30h]			; rax = InInitializationOrderModuleList.Flink 
-	mov			rcx, [rax+10h]			; rcx = DllBase (ntdll.dll)
-	mov			rbp, rcx				; rbp = DllBase (ntdll.dll)
+	mov         rax, gs:[60h]                       ; rax = ProcessEnvironmentBlock
+	mov         rax, [rax+18h]                      ; rax = Ldr
+	mov         rax, [rax+30h]                      ; rax = InInitializationOrderModuleList.Flink 
+	mov         rcx, [rax+10h]                      ; rcx = DllBase (ntdll.dll)
+	mov         rbp, rcx                            ; rbp = DllBase (ntdll.dll)
 
 
-	lea			rdx, [lbl_LdrLoadDll]
+	lea         rdx, [lbl_LdrLoadDll]
 
 	; Look up address for LdrLoadDll()
 	;
-	call		getProcAddressForMod
+	call        getProcAddressForMod
 
-	test		rax, rax
-	jz			@@1						; Skip calling that function if failed to get its pointer
+	test        rax, rax
+	jz          @@1                                 ; Skip calling that function if failed to get its pointer
 
-	mov			r10, rax
+	mov         r10, rax
 
 	; Get length of our DllName
 	;
-	xor			rcx, rcx
-	xor			eax, eax
-	dec			rcx
-	mov			rdx, rdi
+	xor         rcx, rcx
+	xor         eax, eax
+	dec         rcx
+	mov         rdx, rdi
 
-	repne		scasw
+	repne       scasw
 
-	sub			rdi, rdx				; rdi = length of DllName in bytes + sizeof(WCHAR)
+	sub         rdi, rdx                            ; rdi = length of DllName in bytes + sizeof(WCHAR)
 
 
 	; struct UNICODE_STRING {
-	; 	USHORT Length;					[+0h] length of string in bytes (not inclusing last null)
-	; 	USHORT MaximumLength;			[+2h] length of string in bytes (including last null)
-	; 	USHORT* Buffer;					[+8h] pointer to the string
+	; 	USHORT Length;                          [+0h] length of string in bytes (not inclusing last null)
+	; 	USHORT MaximumLength;                   [+2h] length of string in bytes (including last null)
+	; 	USHORT* Buffer;                         [+8h] pointer to the string
 	; };
 
 	; Prepare UNICODE_STRING with our injected DLL name on the stack at [rsp + 20h]
 	;
-	mov			[rsp + 22h], di			; MaximumLength
-	sub			rdi, 2
-	mov			[rsp + 20h], di			; Length
-	mov			[rsp + 28h], rdx		; Buffer
+	mov         [rsp + 22h], di                     ; MaximumLength
+	sub         rdi, 2
+	mov         [rsp + 20h], di                     ; Length
+	mov         [rsp + 28h], rdx                    ; Buffer
 
-	xor			rcx, rcx				; PathToFile = 0
-	xor			rdx, rdx				; Flags = 0
-	lea			r8, [rsp + 20h]			; &ModuleFileName
-	lea			r9,	[rsp + 30h]			; &ModuleHandle
+	xor         rcx, rcx                            ; PathToFile = 0
+	xor         rdx, rdx                            ; Flags = 0
+	lea         r8, [rsp + 20h]                     ; &ModuleFileName
+	lea         r9,	[rsp + 30h]                     ; &ModuleHandle
 
 	; LdrLoadDll(
 	;   IN PWCHAR               PathToFile OPTIONAL,
@@ -112,7 +112,7 @@ UserModeNormalRoutine PROC
 	;   OUT PHANDLE             ModuleHandle
 	; );
 	;
-	call		r10
+	call        r10
 
 
 	; We can't do anything here if LdrLoadDll failed to load Dll
@@ -120,54 +120,54 @@ UserModeNormalRoutine PROC
 
 @@1:
 
-	lea			rdx, [lbl_NtUnmapViewOfSection]
-	mov			rcx, rbp			; rcx = base address of Ntdll.dll
+	lea         rdx, [lbl_NtUnmapViewOfSection]
+	mov         rcx, rbp                            ; rcx = base address of Ntdll.dll
 
 	; Look up address of NtUnmapViewOfSection
 	;
-	call		getProcAddressForMod
+	call        getProcAddressForMod
 
 	; Restore the stack frame & nonvolatile registers
 	;
-	add			rsp, 48h
-	mov			rdi, [rsp + 10h]
-	mov			rbp, [rsp + 18h]
+	add         rsp, 48h
+	mov         rdi, [rsp + 10h]
+	mov         rbp, [rsp + 18h]
 
 
 	; Check the result of getting function ptr
 	;
-	test		rax, rax
-	jz			@@fail
+	test        rax, rax
+	jz          @@fail
 
 
 	; IMPORTANT: We need to invoke NtUnmapViewOfSection via a JMP because it will be freeing the memory
 	;            section that our code runs from, thus we cannot return into it via a CALL instruction!
 	;
-	xor			ecx, ecx
-	mov			rdx, [rsp + 08h]	; rdx = BaseAddr
-	dec			rcx					; rcx = NtCurrentProcess()
+	xor         ecx, ecx
+	mov         rdx, [rsp + 08h]	; rdx = BaseAddr
+	dec         rcx                                 ; rcx = NtCurrentProcess()
 
 	; NTSYSAPI NTSTATUS NtUnmapViewOfSection(
 	;   HANDLE ProcessHandle,
 	;   PVOID  BaseAddress
 	; );
 	;
-	jmp			rax
+	jmp         rax
 
 @@fail:
 	; Failed to unmap
 	;
 	IFDEF MASM_DEBUG
-	int			3				; debugging break (for debug build only)
+	int         3                                   ; debugging break (for debug build only)
 	ENDIF
 
 	ret
 
 
 lbl_LdrLoadDll:
-	db			'LdrLoadDll',0
+	db 'LdrLoadDll',0
 lbl_NtUnmapViewOfSection:
-	db			'NtUnmapViewOfSection',0
+	db 'NtUnmapViewOfSection',0
 
 UserModeNormalRoutine ENDP
 
@@ -185,107 +185,107 @@ getProcAddressForMod PROC PRIVATE
 	;		 = Pointer to function code
 	;		 = 0 if failed
 
-	push		r12
-	push		rbp
-	push		rsi
-	push		rdi
-	push		rbx
+	push        r12
+	push        rbp
+	push        rsi
+	push        rdi
+	push        rbx
 
-	mov			rbx, rcx					; rbx = PIMAGE_DOS_HEADER
+	mov         rbx, rcx                            ; rbx = PIMAGE_DOS_HEADER
 
 	; Calculate length of function name in chars + terminating null
 	;
-	xor			ecx, ecx
-	mov			rdi, rdx
-	dec			rcx
-	xor			al, al
-	repne		scasb
-	sub			rdi, rdx
-	mov			rbp, rdi					; rbp = function name length in chars
+	xor         ecx, ecx
+	mov         rdi, rdx
+	dec         rcx
+	xor         al, al
+	repne       scasb
+	sub         rdi, rdx
+	mov         rbp, rdi                            ; rbp = function name length in chars
 
 	; "Walk" the PE header
 	;
-	movsxd		rax, dword ptr [rbx+3Ch]	; e_lfanew
-	mov			eax, [rbx+rax+88h]			; PIMAGE_DOS_HEADER::OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress
-	add			rax, rbx					; rax = PIMAGE_EXPORT_DIRECTORY
+	movsxd      rax, dword ptr [rbx+3Ch]	; e_lfanew
+	mov         eax, [rbx+rax+88h]                  ; PIMAGE_DOS_HEADER::OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress
+	add         rax, rbx                            ; rax = PIMAGE_EXPORT_DIRECTORY
 
-	mov			r9d, [rax+18h]				; r9 = NumberOfNames = B
-	test		r9d, r9d
-	jz			@@bad
+	mov         r9d, [rax+18h]                      ; r9 = NumberOfNames = B
+	test        r9d, r9d
+	jz          @@bad
 
 	; Get pointers to PE header structures
 	;
-	mov			r12d, [rax+1Ch]				; r12 = AddressOfFunctions
-	mov			r11d, [rax+20h]				; r11 = AddressOfNames
-	mov			r10d, [rax+24h]				; r10 = AddressOfNameOrdinals
-	add			r10, rbx
-	add			r11, rbx
-	add			r12, rbx
+	mov         r12d, [rax+1Ch]                     ; r12 = AddressOfFunctions
+	mov         r11d, [rax+20h]                     ; r11 = AddressOfNames
+	mov         r10d, [rax+24h]                     ; r10 = AddressOfNameOrdinals
+	add         r10, rbx
+	add         r11, rbx
+	add         r12, rbx
 
 
-	xor			r8d, r8d					; A = 0, B = NumberOfNames
+	xor         r8d, r8d                            ; A = 0, B = NumberOfNames
 
 @@1:
 	; Since export function names are sorted alphabetically we'll be using binary search algorithm:
 
 	; Ordinal = (A + B) / 2
 	;
-	lea			eax, [r8d+r9d]
-	shr			eax, 1						; eax = index (or, Ordinal)
+	lea         eax, [r8d+r9d]
+	shr         eax, 1                              ; eax = index (or, Ordinal)
 
-	mov			edi, [r11+rax*4]			; AddressOfNames[Ordinal]
-	add			rdi, rbx
+	mov         edi, [r11+rax*4]                    ; AddressOfNames[Ordinal]
+	add         rdi, rbx
 
-	mov			rcx, rbp					; rcx = name length
-	mov			rsi, rdx					; rsi = function name
+	mov         rcx, rbp                            ; rcx = name length
+	mov         rsi, rdx                            ; rsi = function name
 
 	; Compare function name, letter-by-letter until they are not equal
 	;
-	repe		cmpsb
+	repe        cmpsb
 
-	jnz			@@2
+	jnz         @@2
 
 	; We found a match!
 	;
-	movzx		eax, word ptr [r10+rax*2]	; eax = AddressOfNameOrdinals[Ordinal]
-	mov			eax, dword ptr [r12+rax*4]	; eax = AddressOfFunctions[eax]
-	add			rax, rbx					; rax = needed address of function
+	movzx       eax, word ptr [r10+rax*2]           ; eax = AddressOfNameOrdinals[Ordinal]
+	mov         eax, dword ptr [r12+rax*4]          ; eax = AddressOfFunctions[eax]
+	add         rax, rbx                            ; rax = needed address of function
 
-	jmp			@@out
+	jmp         @@out
 
 @@2:
 	; Depending on the result of comparison
 	;
-	js			@@3
+	js          @@3
 
 	; A = Ordinal + 1
 	;
-	lea			r8d, [eax + 1]
-	jmp			@@4
+	lea         r8d, [eax + 1]
+	jmp         @@4
 
 @@3:
 	; B = Ordinal
 	;
-	mov			r9d, eax
+	mov         r9d, eax
 
 @@4:
 	; Check if we're out of range
 	;
-	cmp			r8d, r9d					; range: [A = r8d, B = r9d)
-	jb			@@1
+	cmp         r8d, r9d                            ; range: [A = r8d, B = r9d)
+	jb          @@1
 
 
 @@bad:
 	; Nothing was found
 	;
-	xor			eax, eax
+	xor         eax, eax
 
 @@out:
-	pop			rbx
-	pop			rdi
-	pop			rsi
-	pop			rbp
-	pop			r12
+	pop         rbx
+	pop         rdi
+	pop         rsi
+	pop         rbp
+	pop         r12
 
 	ret
 
